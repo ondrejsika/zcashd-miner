474762526994211ca8e78e618587e979b20119e5
Ondrej Sika <ondrej@ondrejsika.com>
2018-10-31 10:46:20 +0100

    bump version to v1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zcashd-miner',
-    version='1.1.0',
+    version='1.1.1',
     url='https://github.com/ondrejsika/zcashd-miner',
     license='MIT',
     description='CPU miner for Zcash',
